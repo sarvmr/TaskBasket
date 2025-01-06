49d6d1e732a0638f42bb7c3c12bef391abc0b74f
Sarvenaz Mohammadi <sarv.mr@gmail.com>
2025-01-06 14:44:20 -0800

    initial build and run

---
+++ Data/TaskContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskBasket.Data
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
